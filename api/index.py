5a05ba227813a8d50b93b0554f3b6a0112a7209c
practiceprojects <167779657+practiceprojects1@users.noreply.github.com>
2024-09-12 13:56:45 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index2.html')
-
-def text():
-    return "This is test text"
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
