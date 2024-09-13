43b9b4cd55cb6d014748f2d2bdebfaf98b9ed8e1
practiceprojects <167779657+practiceprojects1@users.noreply.github.com>
2024-09-13 09:00:06 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     global test
-    test1=print(test)
+    test1= "What about this?"
     return render_template('index2.html', test2=str(test1))
     
 
