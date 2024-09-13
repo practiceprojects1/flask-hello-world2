badefe2963d4113a87d6cc6f05683c8175de6312
practiceprojects <167779657+practiceprojects1@users.noreply.github.com>
2024-09-13 07:36:34 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 
+test = ""
+
+def test():
+    global test
+    test = test+"test"
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index2.html')
+    global test
+    return render_template('index2.html', test1=test)
     
 
 if __name__ == '__main__':
+    test()
     app.run(debug=True)
